9c3583b9a091fab5b196bd0f05f35cc086036d99
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-06-03 14:09:30 +0530

    code done.

--- js/index.js
+++ js/index.js
@@ -22,10 +22,10 @@ const Tags = {
     'Summary2': ['lifeLock-summary']
 };
 
-const url = 'https://www.creditscoreiq.com/lander/2-wl/csiq?offercode=431289B0&transactionid=2d3924572582438093e5d97bb4f475f1&offer_id=765&aff_id=1652&aff_sub=%7Bpublisher_or_affid_here%7D&fname=&lname=&email=&address=&city=&state=&zip=&phone=&source=meta&aff_sub2=%7Badditional_sub%7D&aff_sub3=%7Badditional_sub%7D&aff_click_id=%7BclickID_here%7D&utm_campaign=%7Bpublisher_or_affid_here%7D&utm_source=1652&utm_medium=affiliate';
+const url = 'https://protect.identityiq.com/idp/idprotect/ai-scams-single/?offercode=431286X1&goal_id=145&transactionid=bd8f49524d50425293ad8cdf75c4a700&offer_id=828&aff_id=1652&aff_sub=SUB_ID&fname=&lname=&email=&address=&city=&state=&zip=&phone=&source=TRAFFIC_TYPE&aff_sub2=&aff_sub3=&aff_click_id=CLICK_ID&utm_campaign=SUB_ID&utm_source=1652&utm_medium=affiliate&planname=SecureMax+with+Antivirus+And+VPN';
 const url2 = 'https://buy.aura.com/a-save?irclickid=UpUS8A0lhxycTVGQ6qUOJ1wxUksWgST-JQlH3M0&iradname=Online%20Tracking%20Link&iradid=899264&irgwc=1&c1=4944332&camp=12398&utm_source=g2gmedia&utm_medium=ir_affiliate&mktp=ir_affiliate&sharedid=';
 const sum1 = 'https://www.aura.com/legal/insurance-summary-of-benefits';
-const sum2 = 'https://www.nortonlifelock.com/in/en/legal/'
+const sum2 = 'https://www.nortonlifelock.com/in/en/legal/';
 
 Object.entries(Tags).forEach(([tagName, ids]) => {
     ids.forEach((id) => {
